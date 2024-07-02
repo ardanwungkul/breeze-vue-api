a842e237aa283dea21087b5ca72e04804e6f2093
wildan <wildansuhendar87@gmail.com>
2024-07-02 15:20:43 +0700

    anyar

--- src/pages/guest/AboutUs.vue
+++ src/pages/guest/AboutUs.vue
@@ -60,16 +60,16 @@ const swiperConfig = {
     <AppLayout>
         <div class="hidden flex-col w-full h-[calc(100vh-57px)] sm:flex">
             <v-card style="border-radius: 0; box-shadow: none">
-                <div class="w-full min-h-[65vh] flex">
+                <div class="w-full h-[65%] flex">
                     <div
-                        class="flex flex-row w-[60%] 2xl:pl-48 sm:pl-8 min-h-full items-center relative">
+                        class="flex flex-row w-[60%] xl:pl-48 sm:pl-8 min-h-full items-center relative">
                         <div
-                            class="w-full max-h-full absolute top-0 left-0 opacity-20">
+                            class="w-full h-full absolute top-0 -left-10 opacity-20">
                             <v-img
                                 :key="n"
                                 :src="aboutbg"
                                 aspect-ratio="1"
-                                class="h-[65vh]"
+                                class="h-full"
                                 cover>
                                 <template v-slot:placeholder>
                                     <div
@@ -106,7 +106,7 @@ const swiperConfig = {
                             </v-card-text>
                         </div>
                         <div
-                            class="sm:w-2/5 lg:w-1/3 min-h-full absolute sm:top-64 lg:top-44 left-[65%] overflow-hidden">
+                            class="sm:w-2/5 lg:w-1/3 min-h-full absolute sm:top-[20%] lg:top-[20%] left-[65%] overflow-hidden">
                             <v-card-text>
                                 <v-tabs-window v-model="tab">
                                     <v-tabs-window-item
@@ -126,21 +126,16 @@ const swiperConfig = {
                                                         indeterminate></v-progress-circular>
                                                 </div>
                                             </template>
-                                            <div
-                                                style="
-                                                    box-shadow: 0px -10px 10px 20px
-                                                        #ceddce;
-                                                "
-                                                class="absolute left-0 w-full bg-[#ceddce] sm:h-20 lg:h-28 -bottom-1"></div>
                                         </v-img>
                                     </v-tabs-window-item>
                                 </v-tabs-window>
                             </v-card-text>
                         </div>
                     </div>
-                    <div class=" w-[40%] flex flex-row min-h-full bg-[#f8f8f6] xl:pr-48 sm:pr-8 md:pr-8">
-                        <div class=" w-5/6 min-h-full">
-                            <v-card-text class=" !p-0">
+                    <div
+                        class="w-[40%] flex flex-row min-h-full bg-[#f8f8f6] xl:pr-48 sm:pr-8 md:pr-8">
+                        <div class="w-5/6 min-h-full">
+                            <v-card-text class="!p-0">
                                 <v-tabs-window v-model="tab">
                                     <v-tabs-window-item
                                         v-for="i in about"
@@ -168,34 +163,37 @@ const swiperConfig = {
                         <div
                             class="w-1/6 min-h-full flex flex-col py-5 items-center gap-5">
                             <div
-                                v-for="i in 3"
-                                :key="i"
-                                class="w-8 max-h-8 flex justify-center items-center">
-                                <svg
-                                    id="Icons_User"
-                                    overflow="hidden"
-                                    version="1.1"
-                                    viewBox="0 0 96 96"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <g>
-                                        <circle cx="48" cy="30" r="16" />
-                                        <path
-                                            d=" M 80 82 L 80 66 C 80 63.6 78.8 61.2 76.8 59.6 C 72.4 56 66.8 53.6 61.2 52 C 57.2 50.8 52.8 50 48 50 C 43.6 50 39.2 50.8 34.8 52 C 29.2 53.6 23.6 56.4 19.2 59.6 C 17.2 61.2 16 63.6 16 66 L 16 82 L 80 82 Z" />
-                                    </g>
-                                </svg>
+                                class="w-8 h-8 flex justify-center items-center">
+                                <i class="fa-solid fa-user text-[20px]"></i>
+                            </div>
+                            <div
+                                class="w-8 h-8 flex justify-center items-center">
+                                <i class="fa-solid fa-cart-shopping text-[20px]"></i>
+                            </div>
+                            <div
+                                class="w-8 h-8 flex justify-center items-center">
+                                <i class="fa-regular fa-heart text-[20px]"></i>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class=" w-full min-h-[35vh] flex">
-                    <div class=" w-[60%] min-h-full xl:pl-48 sm:pl-8 bg-[#ceddce] gap-5 flex flex-col">
-                        <div class=" flex flex-row sm:gap-5 lg:gap-20 p-5">
-                            <button class=" py-3 px-10 border border-white text-white font-semibold bg-gradient-to-r from-lime-600 to-lime-950 duration-200 hover:opacity-70 rounded-xl">Check Shop</button>
-                            <button class=" font-semibold duration-200 hover:text-gray-500">Learn more -></button>
+                <div class="w-full min-h-[35%] flex">
+                    <div
+                        class="w-[60%] min-h-full xl:pl-48 sm:pl-8 bg-[#ceddce] gap-5 flex flex-col">
+                        <div class="flex flex-row sm:gap-5 lg:gap-20 p-5">
+                            <button
+                                class="py-3 px-10 border border-white text-white font-semibold bg-gradient-to-r from-lime-600 to-lime-950 duration-200 hover:opacity-70 rounded-xl">
+                                Check Shop
+                            </button>
+                            <button
+                                class="font-semibold duration-200 hover:text-gray-500">
+                                Learn more ->
+                            </button>
                         </div>
                         <div
-                            class="w-full flex flex-row static z-10 sm:gap-1 lg:gap-5">
+                            style="
+                            box-shadow: 0px -1px 10px 20px #ceddce"
+                            class="w-full flex flex-row static z-10 sm:gap-1 lg:gap-5 bg-[#ceddce]">
                             <div
                                 class="flex flex-row sm:gap-1 lg:gap-3"
                                 v-for="i in 3"
@@ -228,7 +226,8 @@ const swiperConfig = {
                             </div>
                         </div>
                     </div>
-                    <div class=" w-[40%] min-h-full xl:pr-48 sm:pr-8 md:pr-8 bg-[#f8f8f6] static z-10">
+                    <div
+                        class="w-[40%] min-h-full xl:pr-48 sm:pr-8 md:pr-8 bg-[#f8f8f6] static z-10">
                         <v-tabs
                             style="background-color: #f8f8f6 !important"
                             v-model="tab"
@@ -368,10 +367,18 @@ const swiperConfig = {
                             </v-card-text>
                         </div>
                     </div>
-                    <div class=" flex flex-col w-full h-[45%] bg-[#ceddce] relative">
-                        <div class=" flex flex-col w-full gap-5 sm:gap-5 lg:gap-20 pt-10 py-4 px-5 right-0">
-                            <button class=" py-3 px-10 border border-white text-white font-semibold bg-gradient-to-r from-lime-600 to-lime-950 duration-200 hover:opacity-70 rounded-xl text-sm">Check Shop</button>
-                            <button class=" text-sm font-semibold duration-200 hover:text-gray-500">Learn more -></button>
+                    <div
+                        class="flex flex-col w-full h-[45%] bg-[#ceddce] relative">
+                        <div
+                            class="flex flex-col w-full gap-5 sm:gap-5 lg:gap-20 pt-10 py-4 px-5 right-0">
+                            <button
+                                class="py-3 px-10 border border-white text-white font-semibold bg-gradient-to-r from-lime-600 to-lime-950 duration-200 hover:opacity-70 rounded-xl text-sm">
+                                Check Shop
+                            </button>
+                            <button
+                                class="text-sm font-semibold duration-200 hover:text-gray-500">
+                                Learn more ->
+                            </button>
                         </div>
                         <div class="w-full h-1/2">
                             <v-tabs
@@ -381,9 +388,12 @@ const swiperConfig = {
                                 bg-color="primary"
                                 class="min-h-full w-full flex !bg-[#ceddce]">
                                 <v-tab
-                                    v-for="i in about" :key="i.about" :value="i.index" 
-                                    class=" !bg-[#ceddce] !rounded-xl min-h-full  w-1/3 max-w-1/3 duration-300 hover:opacity-100 opacity-50 overflow-hidden">
-                                    <div class=" !w-full !min-h-full flex flex-col justify-center items-center gap-5 hover:bg-[#ceddce] absolute p-12">
+                                    v-for="i in about"
+                                    :key="i.about"
+                                    :value="i.index"
+                                    class="!bg-[#ceddce] !rounded-xl min-h-full w-1/3 max-w-1/3 duration-300 hover:opacity-100 opacity-50 overflow-hidden">
+                                    <div
+                                        class="!w-full !min-h-full flex flex-col justify-center items-center gap-5 hover:bg-[#ceddce] absolute p-12">
                                         <v-img
                                             :src="i.img"
                                             aspect-ratio="1"
---
+++ src/pages/guest/product/Show.vue
@@ -0,0 +1,182 @@
+<script setup>
+import AppLayout from '@/layouts/AppLayout.vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Navigation, Autoplay } from 'swiper/modules'
+import { Tabs } from 'flowbite'
+import 'swiper/css'
+
+import cosmetic from '@/assets/images/cosmetic.jpeg'
+
+const swiperModules = [Navigation, Autoplay]
+const swiperJs = swiper => {}
+const swiperConfig = {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+}
+const modules = swiperModules
+</script>
+<template>
+    <AppLayout>
+        <div class="flex flex-row w-[1120px] bg-white mx-auto">
+            <div class="flex flex-col gap-2 w-[40%] h-full py-6 p-4">
+                <div class="w-full h-96 rounded-sm">
+                    <v-img
+                        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F010%2F175%2F452%2Fnon_2x%2Fwhite-cream-jar-bottle-beauty-cosmetic-blank-mockup-3d-illustration-free-png.png&f=1&nofb=1&ipt=fa54a50dcaf23d1ca8ded9f4daed07a9259ee9373c9933b46731843ffd37b128&ipo=images"
+                        aspect-ratio="1">
+                        <template v-slot:placeholder>
+                            <div
+                                class="w-full h-full flex justify-center items-center">
+                                <v-progress-circular
+                                    color=""
+                                    indeterminate></v-progress-circular>
+                            </div>
+                        </template>
+                    </v-img>
+                </div>
+                <div class="w-full h-20">
+                    <swiper
+                        :modules="swiperModules"
+                        :breakpoints="{
+                            '640': {
+                                slidesPerView: 5,
+                                spaceBetween: 2,
+                            },
+                            '768': {
+                                slidesPerView: 5,
+                                spaceBetween: 2,
+                            },
+                            '1024': {
+                                slidesPerView: 5,
+                                spaceBetween: 2,
+                            },
+                        }"
+                        @swiper="swiperJs"
+                        :navigation="swiperConfig.navigation">
+                        <swiper-slide v-for="n in 6" :key="n">
+                            <div class="w-full h-full">
+                                <v-img
+                                    src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F010%2F175%2F452%2Fnon_2x%2Fwhite-cream-jar-bottle-beauty-cosmetic-blank-mockup-3d-illustration-free-png.png&f=1&nofb=1&ipt=fa54a50dcaf23d1ca8ded9f4daed07a9259ee9373c9933b46731843ffd37b128&ipo=images"
+                                    aspect-ratio="1">
+                                    <template v-slot:placeholder>
+                                        <div
+                                            class="w-full h-full flex justify-center items-center">
+                                            <v-progress-circular
+                                                color=""
+                                                indeterminate></v-progress-circular>
+                                        </div>
+                                    </template>
+                                </v-img>
+                            </div>
+                        </swiper-slide>
+                        <div
+                            class="flex absolute top-0 w-full h-full justify-between z-20 items-center">
+                            <div
+                                class="swiper-button-prev bg-black/25 py-3 px-1 flex justify-center items-center cursor-pointer">
+                                <i
+                                    class="fa-solid fa-chevron-left text-white"></i>
+                            </div>
+                            <div
+                                class="swiper-button-next bg-black/25 py-3 px-1 flex justify-center items-center cursor-pointer">
+                                <i
+                                    class="fa-solid fa-chevron-right text-white"></i>
+                            </div>
+                        </div>
+                    </swiper>
+                </div>
+                <!-- <div class=" px-5 flex flex-row w-full h-6 items-center gap-2">
+                    <p>Share :</p>
+                    <div v-for="i in 4" :key="i" class=" w-6 h-6 bg-black rounded-full overflow-hidden"></div>
+                </div> -->
+            </div>
+            <div class="w-[60%] h-full py-6 p-4 gap-2 flex flex-col">
+                <p class="text-xl font-medium">Skincar Wajah</p>
+                <div class="flex flex-row items-center gap-2">
+                    <p
+                        class="text-xs underline underline-offset-4 text-gray-500">
+                        5.0
+                    </p>
+                    <div class="flex flex-row gap-0.5">
+                        <i
+                            v-for="n in 5"
+                            :key="n"
+                            class="fa-solid fa-star text-yellow-300 text-xs"></i>
+                    </div>
+                </div>
+                <div
+                    class="flex justify-between w-full py-1 px-5 bg-red-600 text-white items-center">
+                    <p class="tracking-wider text-lg font-medium">
+                        <span class="font-semibold">Flash</span>Sale
+                    </p>
+                    <div class="flex flex-row gap-2">
+                        <p>BERAKHIR DALAM</p>
+                        <div class="flex flex-row gap-1 font-medium">
+                            <p class="bg-black px-1">00</p>
+                            <p class="bg-black px-1">00</p>
+                            <p class="bg-black px-1">00</p>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="flex w-full bg-[#f4f4f4] flex-row gap-3 py-3 -mt-2 px-5">
+                    <p class="line-through text-gray-500">Rp.200.000</p>
+                    <p class="text-2xl font-medium">Rp.50.000</p>
+                </div>
+                <div class="flex flex-col w-full py-7 px-5 gap-6 text-[13px]">
+                    <div class="flex flex-row w-full">
+                        <div class="w-1/5 min-w-[20%]">
+                            <p class="text-gray-600">Voucher</p>
+                        </div>
+                        <div class="w-auto"></div>
+                    </div>
+                    <div class="flex flex-row w-full">
+                        <div class="w-1/5 min-w-[20%]">
+                            <p class="text-gray-600">Return</p>
+                        </div>
+                        <div class="w-auto">Bebas Pengembalian</div>
+                    </div>
+                    <div class="flex flex-row w-full">
+                        <div class="w-1/5 min-w-[20%]">
+                            <p class="text-gray-600">Protection</p>
+                        </div>
+                        <div class="w-auto flex flex-row">
+                            <p class="w-1/5 flex flex-wrap">
+                                Proteksi Kerusakan+
+                            </p>
+                            <p class="w-5/5 flex flex-wrap text-blue-600">
+                                Melindungi Barang Belanjaanmu dari Kerusakan
+                                total hingga 6 bulan
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex flex-row w-full">
+                        <div class="w-1/5 min-w-[20%]">
+                            <p class="text-gray-600">Kuantitas</p>
+                        </div>
+                        <div class="w-auto flex flex-row gap-2">
+                            <input
+                                class="h-7 w-16 text-sm focus:ring-transparent border-gray-300 focus:border-gray-300"
+                                type="number"
+                                name=""
+                                id="" />
+                            <p class="text-gray-600 flex items-center">
+                                tersisa 458 buah
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-row text-[13px] px-5 gap-2">
+                    <button
+                        class="py-3 px-4 rounded-sm bg-red-100 border border-red-600 text-red-600 hover:bg-red-50 duration-300">
+                        <p>Masukkan Keranjang</p>
+                    </button>
+                    <button
+                        class="py-3 px-4 rounded-sm bg-red-600 text-white hover:bg-red-500 duration-300">
+                        <p>Beli Sekarang</p>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
